d5d308aca14e63d78a5d7e81717939a865b04ea0
DeadManChest <cdoxal@gmail.com>
2025-12-29 16:16:00 +0100

    Add files via upload

---
+++ PS_ListUser_RDPCache.ps1
@@ -0,0 +1,39 @@
+# Demander à l'utilisateur le chemin de la source à analyser
+$Source = Read-Host "Indiquez le chemin du C:\ de la source à analyser (ex: D:\collect\...)"
+
+IF ($Source -eq ""){$Source = "D:\251224\Data\z_Collection_P00-W19-HPV01-wsi-local_C-_2025-12-24T11-10-32Z"}
+$UsersRoot = $Source + "\uploads\auto\C%3A\users"
+
+$Results = @()
+
+Get-ChildItem -Path $UsersRoot -Directory -ErrorAction SilentlyContinue | ForEach-Object {
+
+    $UserName = $_.Name
+    $CachePath = Join-Path $_.FullName "AppData\Local\Microsoft\Terminal Server Client\Cache"
+
+    if (Test-Path $CachePath) {
+        $FileCount = (Get-ChildItem -Path $CachePath -File -ErrorAction SilentlyContinue).Count
+    }
+    else {
+        $FileCount = 0
+    }
+
+    $Results += [PSCustomObject]@{
+        Utilisateur = $UserName
+        NbFichiers  = $FileCount
+        CachePath   = $CachePath
+
+    }
+
+if ($FileCount -gt 0)
+	{
+	write-host -fore Green $UserName : $FileCount;
+	Write-host -fore Blue "Appuyez sur une touche Ouvrir l'explorer"
+	pause
+	Start-Process explorer.exe $CachePath
+	}
+}
+
+# Affichage tableau
+$Results | Format-Table -AutoSize
+
